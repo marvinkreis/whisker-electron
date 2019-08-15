cb05dfc35a60961a0168cc401cd459ce30bbc32e
Marvin Kreis <MarvinKreis@web.de>
2019-08-16 01:31:38 +0200

    Make main functions not object oriented and add error handling

--- src/main/index.js
+++ src/main/index.js
@@ -3,97 +3,132 @@ const path = require('path');
 const {app, BrowserWindow, ipcMain} = require('electron');
 const minimist = require('minimist');
 
-class Main {
-  constructor (testsPath, projectPaths, outdir) {
-    this.testsCode = fs.readFileSync(testsPath, 'utf8');
-    this.projectPaths = projectPaths;
-    this.outdir = outdir;
-    ipcMain.on('testsDone', this.onTestsDone.bind(this))
-  }
+let testsCode;
+let outDir;
+let projectPaths;
 
-  async runTests () {
-    for (const projectPath of this.projectPaths) {
-      await this.runTestsOnProject(projectPath);
+const setAndValidateOptions = function () {
+    const error = message => {
+        console.error(message);
+        app.quit();
+        process.exit(1);
+    };
+
+    const argv = minimist(process.argv.slice(2), {default: {outdir: '.'}});
+
+    if (argv._.length === 0 || argv.tests === undefined) {
+        error(`Usage: ${process.argv[1]} --tests testsFile [--outdir dir] [projectFile...]`);
     }
-  }
 
-  async runTestsOnProject (projectPath) {
-    process.stdout.write(`Running tests on ${projectPath} ...`);
+    const testsPath = argv.tests;
+    outDir = argv.outdir;
+    projectPaths = argv._;
+
+    try {
+        const stat = fs.statSync(testsPath);
+        if (!stat.isFile()) {
+            error(`Tests file "${testsPath}" is not a directory.`);
+        }
+        testsCode = fs.readFileSync(testsPath, 'utf8');
+    } catch (err) {
+        if (err.code !== 'ENOENT') throw err;
+        error(`Tests file does not exist "${testsPath}`);
+    }
+
+    try {
+        const stat = fs.statSync(outDir);
+        if (!stat.isDirectory()) {
+            error(`Output directory "${outDir}" is not a directory.`);
+        }
+    } catch (err) {
+        if (err.code !== 'ENOENT') throw err;
+        error(`Output directory "${outDir}" does not exist.`);
+    }
+
+    for (const projectPath of projectPaths) {
+        try {
+            const stat = fs.statSync(projectPath);
+            if (!stat.isFile()) {
+                error(`Project directory "${projectPath}" is not a directory.`);
+            }
+        } catch (err) {
+            if (err.code !== 'ENOENT') throw err;
+            error(`Project file "${projectPath}" does not exist.`);
+        }
+    }
+};
+
+const runTestsOnProject = async function (projectPath) {
+    console.log(`Running tests on ${projectPath} ...`);
 
     let resolve;
-    const promise = new Promise((rs, rj) => {
-      resolve = rs;
+    const promise = new Promise((rs, _) => {
+        resolve = rs;
     });
 
     const project = fs.readFileSync(projectPath);
 
     const window = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        nodeIntegration: true
-      },
-      autoHideMenuBar: true
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        autoHideMenuBar: true
     });
 
     window.loadURL(`file://${__dirname}/../../static/index.html`);
 
     window.webContents.on('did-finish-load', () => {
-      window.webContents.send('runTests', {
-        testsCode: this.testsCode,
-        project: project,
-        id: projectPath
-      });
+        window.webContents.send('runTests', {
+            testsCode: testsCode,
+            project: project,
+            id: projectPath
+        });
     });
 
     window.on('closed', () => {
-      process.stdout.write(' done\n');
-      resolve();
+        console.log();
+        resolve();
     });
 
     return promise;
-  }
+};
 
-  onTestsDone (event, message) {
-    let {id, tap13Report} = message;
-    const parse = path.parse(id);
-    console.log(parse);
-    tap13Report = [`# project: ${parse.base}`, tap13Report].join('\n');
-    const outpath = path.resolve(this.outdir, parse.name + '.tap');
-    fs.writeFileSync(outpath, tap13Report);
-  }
-}
+const onTestsDone = function (event, message) {
+    const {id, tap13Report, summary, coverage} = message;
+    console.log(`Finished running tests on "${id}"`);
+    console.log(summary);
+    console.log(coverage);
+
+    const pathInfo = path.parse(id);
+    const outString = `# project: ${pathInfo.base}\n${tap13Report}}`;
+    const outPath = path.resolve(outDir, pathInfo.name + '.tap');
+
+    try {
+        fs.writeFileSync(outPath, outString);
+    } catch (e) {
+        error(`Could not write test report "${outPath}".`);
+    }
+};
+
+const onTestsError = function (event, message) {
+    console.error(`Error while running tests on "${message.id}":\n${message.message}`);
+};
+
+const main = async function () {
+    setAndValidateOptions();
+
+    ipcMain.on('testsDone', onTestsDone);
+    ipcMain.on('testsError', onTestsError);
+
+    for (const projectPath of projectPaths) {
+        await runTestsOnProject(projectPath);
+    }
+
+    app.quit()
+};
 
 /* Don't quit when a test run finished. */
 app.on('window-all-closed', e => e.preventDefault());
-
-app.on('ready', async () => {
-  const argv = minimist(process.argv.slice(2));
-
-  if (argv._.length < 2) {
-    console.error(`Usage: ${process.argv[1]} --tests testsFile [--outdir dir] [projectFile...]`);
-    app.quit();
-    process.exit(1);
-  }
-
-  const projectPaths = argv._;
-
-  let testsPath;
-  if (argv.tests !== undefined) {
-    testsPath = argv.tests;
-  } else {
-    console.error(`Usage: ${process.argv[1]} --tests testsFile [--outdir dir] [projectFile...]`);
-    app.quit();
-    process.exit(1);
-  }
-
-  let outdir = '.';
-  if (argv.outdir !== undefined) {
-    outdir = argv.outdir;
-  }
-
-  const main = new Main(testsPath, projectPaths, outdir);
-  await main.runTests();
-  app.quit()
-});
-
+app.on('ready', main);
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,58 +1,64 @@
 const {ipcRenderer} = require('electron');
 
-const {Thread} = window.Scratch;
 const {CoverageGenerator, TestRunner, TAP13Listener} = require('../../whisker-main');
+const ScratchStage = require('../src/renderer/scratch-stage');
+const {Thread} = window.Scratch;
 
-const Scratch = require('../src/renderer/scratch-stage');
+let scratch;
 
-class Main {
-    constructor (canvas) {
-        this.scratch = new Scratch(canvas);
-    }
+const loadTestsFromString = function (string) {
+    /* eslint-disable-next-line no-eval */
+    let tests = eval(`${string}; module.exports;`);
+    tests = TestRunner.convertTests(tests);
+    return tests;
+};
 
-    static loadTestsFromString (string) {
-        /* eslint-disable-next-line no-eval */
-        let tests = eval(`${string}; module.exports;`);
-        tests = TestRunner.convertTests(tests);
-        return tests;
-    }
+const runTestsWithCoverage = async function (project, tests) {
+    await scratch.vm.loadProject(project);
+    CoverageGenerator.prepareThread(Thread);
+    CoverageGenerator.prepare(scratch.vm);
+    const testRunner = new TestRunner();
 
-    async runTestsWithCoverage (project, tests) {
-        await this.scratch.vm.loadProject(project);
-        CoverageGenerator.prepareThread(Thread);
-        CoverageGenerator.prepare(this.scratch.vm);
-        const testRunner = new TestRunner();
+    const tap13Report = [];
+    const tap13Listener = new TAP13Listener(testRunner, tap13Report.push.bind(tap13Report));
 
-        const tap13Report = [];
-        const tap13Listener = new TAP13Listener(testRunner, tap13Report.push.bind(tap13Report));
+    const summary = await testRunner.runTests(scratch.vm, project, tests);
+    const coverage = CoverageGenerator.getCoverage();
+    const coveragePerSprite = coverage.getCoveragePerSprite();
 
-        const summary = await testRunner.runTests(this.scratch.vm, project, tests);
-        const coverage = CoverageGenerator.getCoverage();
-        const coveragePerSprite = coverage.getCoveragePerSprite();
+    CoverageGenerator.restoreThread(Thread);
 
-        CoverageGenerator.restoreThread(Thread);
+    const formattedSummary = TAP13Listener.formatSummary(summary);
+    const formattedCoverage = TAP13Listener.formatCoverage(coveragePerSprite);
+    const summaryString = TAP13Listener.extraToYAML({summary: formattedSummary});
+    const coverageString = TAP13Listener.extraToYAML({coverage: formattedCoverage});
+    tap13Report.push(summaryString);
+    tap13Report.push(coverageString);
 
-        const formattedSummary = TAP13Listener.formatSummary(summary);
-        const formattedCoverage = TAP13Listener.formatCoverage(coveragePerSprite);
-        const summaryString = TAP13Listener.extraToYAML({summary: formattedSummary});
-        const coverageString = TAP13Listener.extraToYAML({coverage: formattedCoverage});
-        tap13Report.push(summaryString);
-        tap13Report.push(coverageString);
+    return {
+        summary,
+        coverage: coveragePerSprite,
+        tap13Report: tap13Report.join('\n')
+    };
+};
 
-        return {
-            summary,
-            coverage,
-            tap13Report: tap13Report.join('\n')
-        };
-    }
-}
+const main = function () {
+  scratch = new ScratchStage(document.getElementById('scratch-stage'));
+};
 
-const main = new Main(document.getElementById('scratch-stage'));
+document.addEventListener("DOMContentLoaded", main);
 
 ipcRenderer.on('runTests', async (event, message) => {
     const {testsCode, project, id} = message;
-    const tests = Main.loadTestsFromString(testsCode);
-    const {summary, coverage, tap13Report} = await main.runTestsWithCoverage(project, tests);
-    ipcRenderer.send('testsDone', {summary, coverage, tap13Report, id});
+
+    const tests = loadTestsFromString(testsCode);
+
+    try {
+        const {summary, coverage, tap13Report} = await runTestsWithCoverage(project, tests);
+        ipcRenderer.send('testsDone', {summary, coverage, tap13Report, id});
+    } catch (e) {
+        ipcRenderer.send('testsError', {id, message: e.message});
+    }
+
     window.close();
 });
